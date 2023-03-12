f0134206cba1e5dc6a2faf108349ec5e2ab89536
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-03-12 12:31:25 +0500

    t01

--- task01.py
+++ task01.py
@@ -8,4 +8,22 @@ def remove_spaces(string: str)-> str:
         str: string without spaces
     """
     # Your code here
-    pass
+    ans = ""
+    idx = 0
+    if string[0]!=" ":
+        ans+=string[0]
+    idx=+1
+    if string[1]!=" ":
+        ans+=string[1]
+    idx+=1
+    if string[2]!=" ":
+        ans+=string[2]
+    idx+=1
+    if string[3]!=" ":
+        ans+=string[3]
+    idx=+1
+    if string[4]!=" ":
+        ans+=string[4]
+    idx+=1
+    return ans
+print(remove_spaces('a b c'))
